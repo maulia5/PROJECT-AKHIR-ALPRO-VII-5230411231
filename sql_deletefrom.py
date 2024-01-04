0617d1a00cf68ba9d15475d2d401bef47ff3b7cc
maulia5 <mauliahafifatunsolihah@gmail.com>
2024-01-04 15:28:57 +0700

    First Commit

---
+++ sql_deletefrom.py
@@ -0,0 +1,10 @@
+#Menghapus isian field jenis=mamalia
+import sqlite3
+koneksi = sqlite3.connect('database_hewan.db')
+kursor = koneksi.cursor()
+
+kursor.execute(f"DELETE FROM HEWAN WHERE jenis = 'Mamalia'")
+koneksi.commit()
+
+print("Berhasil Dihapus")
+koneksi.close()
